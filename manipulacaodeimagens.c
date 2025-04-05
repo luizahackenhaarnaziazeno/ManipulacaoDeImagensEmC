0240a473b4722c4b0442195b6a812268f79aacbb
Luiza Hackenhar Naziazeno <142232479+luizahackenhaarnaziazeno@users.noreply.github.com>
2025-04-04 23:46:31 -0300

    ajustar a leitura da pasta das imagens

--- manipulacaodeimagens.c
+++ manipulacaodeimagens.c
@@ -1,13 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char nomeImagem[100]= "C:\projects\Facul\T1C\ManipulacaoDeImagens\Imagens";
+char imagem[100];
+
+void tonsDeCinza();
+void negativa();
+void raioX();
+void envelhecida();
+void rotacionar90();
+void rotacionar180();
 
 int main()
 {
     int opcao;
-    /*char imageName[100];
-    printf("Menu de Escolha da Imagem:\n");
-    printf("Digite o nome da imagem PPM que deseja manipular (com extensão .ppm): ");
-    scanf("%s", imageName);
-    printf("Imagem selecionada: %s\n", imageName);*/
+    
+    printf("Imagens Disponiveis: arara vermelha.ppm, cat.ppm, Elephant.ppm, fish.ppm, snake.ppm,tigre branco.ppm\n");
+    printf("Digite o nome da imagem PPM que deseja manipular (Exemplo: imagem.ppm): ");  
+    scanf("%s",imagem);
+    
     do
     {
         printf("Menu Do Que Fazer Com A Imagem:\n");
@@ -24,6 +36,7 @@ int main()
         if (opcao == 1)
         {
             printf("Você escolheu gerar uma imagem tons de cinza.\n");
+            tonsDeCinza();
         }
         else if (opcao == 2)
         {
@@ -55,3 +68,76 @@ int main()
         }
     } while (opcao != 7);
 }
+
+void tonsDeCinza()
+{
+    FILE *fp = fopen(nomeImagem,"r");
+    if (fp == NULL)
+    {
+        perror("Erro ao abrir o arquivo");
+        return;
+    }
+    if (fp == NULL)
+    {
+        printf("Erro ao abrir o arquivo '%s'!\n", nomeImagem);
+        return;
+    }
+
+    char tipoImg[3];
+    int linha, coluna, val, r, g, b, i, j;
+
+    fscanf(fp, "%s", tipoImg);
+    fscanf(fp, "%d %d", &coluna, &linha);
+    fscanf(fp, "%d", &val);
+
+    int matriz[700][700];
+
+    for (j = 0; j < linha; j++)
+    {
+        for (i = 0; i < coluna; i++)
+        {
+            fscanf(fp, "%d %d %d", &r, &g, &b);
+            matriz[j][i] = (int)((r * 0.30) + (g * 0.59) + (b * 0.11));
+        }
+    }
+
+    fclose(fp);
+    printf("Imagem foi gerada com sucesso.\n");
+
+    char nomeSaida[120];
+    strcpy(nomeSaida, nomeImagem);
+    char *ponto = strrchr(nomeSaida, '.');
+    if (ponto != NULL)
+        *ponto = '\0';
+
+    strcat(nomeSaida, "_cinza.ppm");
+
+    FILE *fp_novo = fopen(nomeSaida, "w");
+    if (fp_novo == NULL)
+    {
+        printf("Erro ao criar o novo arquivo!\n");
+        return;
+    }
+
+    fprintf(fp_novo, "P2\n");
+    fprintf(fp_novo, "%d %d\n", coluna, linha);
+    fprintf(fp_novo, "%d\n", val);
+
+    for (j = 0; j < linha; j++)
+    {
+        for (i = 0; i < coluna; i++)
+        {
+            fprintf(fp_novo, "%d\n", matriz[j][i]);
+        }
+    }
+
+    fclose(fp_novo);
+    printf("Nova imagem salva como '%s'.\n", nomeSaida);
+}
+
+// IMPLEMENTAR AS FUNÇÕES AQUI
+void negativa() {}
+void raioX() {}
+void envelhecida() {} //Gabrielle
+void rotacionar90() {} //Luiza 
+void rotacionar180() {} //Luiza
